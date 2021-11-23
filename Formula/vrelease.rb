bf23a2776fce43b88373b82a8fa0ff286c83368f
Caian Ertl <hi@caian.org>
2021-11-23 04:21:56 -0300

    build: bump version

--- Formula/vrelease.rb
+++ Formula/vrelease.rb
@@ -3,8 +3,8 @@ class Vrelease < Formula
   homepage 'https://github.com/vrelease'
   license 'CC0-1.0'
 
-  url 'https://github.com/vrelease/vrelease/releases/download/v0.3.1/vrelease-macos'
-  sha256 'bdfa49eaed3b9f88f57e878e37f7126a1f183cc3b4cbcd41437ea65ca04db4c3'
+  url 'https://github.com/vrelease/vrelease/releases/download/v0.3.2/vrelease-macos'
+  sha256 '9b36c88fbea8481af5321d5e4bf50894e48ebc4c79a683259ee744e1536d7fae'
 
   def install
     bin.install 'vrelease-macos' => 'vrelease'
